d2665ef57f8d747be159ff39cf5c69497b939d50
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-02-18 10:16:37 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+const jwt = require("jsonwebtoken");
+
 const encrypt = (payload, secret) => {
   // your code here and return token
+  const token = jwt.sign(payload, secret, { expiresIn: "1d" });
+  return token;
 };
 
-module.exports = encrypt;
+const decrypt = (token) => {
+  // return decoded payload
+  const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  if (decoded) {
+    return decoded;
+  } else {
+    return false;
+  }
+};
+
+module.exports = {
+  encrypt,
+  decrypt,
+};
